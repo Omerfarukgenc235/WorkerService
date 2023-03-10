0285c36a19f80e97a3989a3999cf576f80bae974
Acer <Acer@LAPTOP-7ABFHQF9>
2023-03-10 23:12:54 +0300

    Proje dosyası ekle.

---
+++ SimpleWorkerService/Worker.cs
@@ -0,0 +1,32 @@
+﻿namespace SimpleWorkerService
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private int sayac;
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation($"Worker Service {sayac} saniyeden beri ayaktadır.");
+                sayac++;
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Worker Servis Başlatıldı...");
+            return base.StartAsync(cancellationToken);
+        }
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Worker Servis Durduruldu...");
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
